89813c0f352b43ea4ac512684d9998f93bcac583
pajith551@gmail.com <pajith551@gmail.com>
2022-04-24 11:50:31 +0530

    user router completed

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MONGO_URL"))
 .catch(err=>console.log(err));
 
+app.use("/api", authRoute);
+app.use("/api/user", userRoute);
 
 app.listen("5000", () => {
     console.log("Server started in Port number 5000");
