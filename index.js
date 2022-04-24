a8f091541c76bec9de1cb088b98ad8168e95f705
pajith551@gmail.com <pajith551@gmail.com>
2022-04-24 20:07:41 +0530

    gitignore is added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
+const PORT = process.env.PORT || 5000;
 
 
 dotenv.config();
@@ -35,6 +36,6 @@ app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen("5000", () => {
+app.listen(PORT, () => {
     console.log("Server started in Port number 5000");
 });
