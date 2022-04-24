84d422ae4a10237540d5e7846e273ead990489af
pajith551@gmail.com <pajith551@gmail.com>
2022-04-24 16:37:00 +0530

    categories created

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 dotenv.config();
 app.use(express.json());
@@ -12,9 +13,10 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MONGO_URL"))
 .catch(err=>console.log(err));
 
-app.use("/api", authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () => {
     console.log("Server started in Port number 5000");
