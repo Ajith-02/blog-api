a835a3003559960c9c9f6f214311220a3cf04496
pajith551@gmail.com <pajith551@gmail.com>
2022-04-24 17:23:17 +0530

    api completed

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
 
 dotenv.config();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to MONGO_URL"))
 .catch(err=>console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) =>{
+        // cb(null, "pic.jpg");
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"),(req, res)=>{
+    res.status(200).json("file had been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
